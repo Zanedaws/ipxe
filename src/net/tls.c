8f9eca89b8f16ae54b5bc940be4f0f980355c850
drewlew13 <lewis446@purdue.edu>
2022-04-14 16:18:23 -0400

    error

--- src/net/tls.c
+++ src/net/tls.c
@@ -825,9 +825,9 @@ static int tls_set_cipher ( struct tls_connection *tls,
 		return -ENOMEM_CONTEXT;
 	}
 
-	DBGC( tls, "TLS %p: Pubkey context size: %u\n", tls, pubkey->ctxsize);
-	DBGC( tls, "TLS %p: Cipher context size: %u\n", tls, cipher->ctxsize);
-	DBGC( tls, "TLS %p: Digest context size: %u\n", tls, digest->ctxsize);
+	DBGC( tls, "TLS %p: Pubkey context size: %zu\n", tls, pubkey->ctxsize);
+	DBGC( tls, "TLS %p: Cipher context size: %zu\n", tls, cipher->ctxsize);
+	DBGC( tls, "TLS %p: Digest context size: %zu\n", tls, digest->ctxsize);
 
 
 	/* Assign storage */
